f8fc086381cd25384a4bec7186e7e4b95c48eb69
heeyjinny <heeyjinny@gmail.com>
2022-11-17 04:19:17 +0900

    소스코드 처리하여 카메라 권한 승인요청 창 띄우기

--- app/src/main/java/com/heeyjinny/permission/MainActivity.kt
+++ app/src/main/java/com/heeyjinny/permission/MainActivity.kt
@@ -61,10 +61,8 @@ class MainActivity : AppCompatActivity() {
         //설정파일(AndroidManifest.xml)을 수정한 다음 소스코드에도 추가로 처리함
 
         //1-1
+        //buid.gradle에서 뷰바인딩 설정하고 사용할 registerForActivityResult의 의존성 확인
         //설정파일(AndroidManifest.xml) 명세하기
-        //bulid.gradle(Module) - 뷰바인딩 설정 - registerForActivityResult 관련 의존성 추가(모르겠음 ㅜㅜ)
-
-        //1-2
         //app - manifests 디렉터리 밑 - AndroidManifest.xml - 카메라 권한 추가
 
         //2
